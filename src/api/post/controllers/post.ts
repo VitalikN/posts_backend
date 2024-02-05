897c87da98e4cccf0c77f1dd652d75fce6292672
vitalik.nozhenko@gmail.com <109283146+VitalikN@users.noreply.github.com>
2024-02-05 17:05:26 +0200

    fix create

--- src/api/post/controllers/post.ts
+++ src/api/post/controllers/post.ts
@@ -1,17 +1,20 @@
 /**
  * post controller
  */
+import { factories } from "@strapi/strapi";
 
-const { createCoreController } = require("@strapi/strapi").factories;
+export default factories.createCoreService("api::post.post");
 
-module.exports = createCoreController("api::post.post", async ({ strapi }) => {
-  return {
-    async create(ctx) {
-      // some logic here
-      //  const user = ctx.state.user;
-      const response = await super.create(ctx);
+// const { createCoreController } = require("@strapi/strapi").factories;
 
-      return response;
-    },
-  };
-});
+// module.exports = createCoreController("api::post.post", async ({ strapi }) => {
+//   return {
+//     async create(ctx) {
+//       // some logic here
+//       //  const user = ctx.state.user;
+//       const response = await super.create(ctx);
+
+//       return response;
+//     },
+//   };
+// });
